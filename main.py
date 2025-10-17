72e3cc1cfff0ee43d47ac4e130c08fddc420439d
princesingh-ml <work.princedeora@gmail.com>
2025-10-17 23:02:32 +0530

    Implement currency exchange rate retrieval from x-rates.com

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from x-rates-scraper!")
+from bs4 import BeautifulSoup
+import requests
 
+currency = input("Currency: ")
+date = input("Date (YYYY-MM-DD): ")
+url = f"https://www.x-rates.com/historical/?from={currency}&amount=1&date={date}"
+response = requests.get(url)
+x_rate_webpage = response.text
 
-if __name__ == "__main__":
-    main()
+soup = BeautifulSoup(x_rate_webpage, "html.parser")
+
+table = soup.find(name= "table", class_ = "tablesorter ratesTable")
+
+if table:
+    rows = table.find_all("tr")
+    for row in rows:
+        col = [td.get_text(strip=True) for td in row.find_all("td")]
+        if col:
+            print(col)
+else:
+    print("No table found.")
